b7670e14d6304b87bbb3e1a2b9b174da69340b83
Eben Sorkin <sorkineben@gmail.com>
2019-02-18 15:32:04 -0500

    Updated middle weights in masters to see if that fixed VF gen issue

--- sources/build.sh
+++ sources/build.sh
@@ -3,17 +3,17 @@ set -e
 
 cd sources
 
-# echo "Generating Static fonts"
-# mkdir -p ../fonts/ttfs
-# fontmake -g "MerriweatherSans for VF.glyphs" -i -o ttf --output-dir ../fonts/ttfs/
-# fontmake -g "MerriweatherSans-Italic for VF.glyphs" -i -o ttf --output-dir ../fonts/ttfs/
+echo "Generating Static fonts"
+mkdir -p ../fonts/ttfs
+fontmake -g "MerriweatherSans for VF.glyphs" -i -o ttf --output-dir ../fonts/ttfs/
+fontmake -g "MerriweatherSans-Italic for VF.glyphs" -i -o ttf --output-dir ../fonts/ttfs/
 
 echo "Generating VFs"
 mkdir -p ../fonts/variable
 fontmake -g "MerriweatherSans for VF.glyphs" -o variable --output-path ../fonts/variable/MerriweatherSans-Roman-VF.ttf
 fontmake -g "MerriweatherSans-Italic for VF.glyphs" -o variable --output-path ../fonts/variable/MerriweatherSans-Italic-VF.ttf
 
-rm -rf master_ufo/ instance_ufo/
+# rm -rf master_ufo/ instance_ufo/
 echo "Post processing"
 
 
